718624592ef7b8cfd9b58dcba0a1a7ffd05940a0
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-01 10:04:41 -0500

    Add cards to admin dashboard showing the number of products and categories in database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USER_NAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+    @productsCount = Product.count
+    @categoriesCount = Category.count
   end
 end
