a25a0cbc264e77bc0b8e85e1e63a5ad5e0010cd7
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 10:35:24 -0500

    Add spec to check validation of new product name

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'validates the presence of a name' do
+      @category = Category.new(name: "Test Category")
+      @category.save
+      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "../../db/seed_assets/apparel1.jpg", price: 2000)
+      @product.save!
+
+      expect(@product).to have_attributes(:name => "Test Product")
+    end
+
+    
+  end
+end
