2df3a4ba89395855566d1839fc6ed31a2415fd2b
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 14:55:42 -0500

    Confirm validation for existing emails is case insensitive

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,5 +19,15 @@ RSpec.describe User, type: :model do
 
       expect(@user2.errors.messages[:email]).to eq ["has already been taken"]
     end
+
+    it 'validates that a new user cannot be created if the provided email contains capital letters, and the email exists in the database in lower case' do
+      @user = User.new(name: "Test Name", last_name: "Test Last Name", email: "TEST@email.com", password: "TestPassword", password_confirmation: "TestPassword")
+      @user.save
+
+      @user2 = User.new(name: "Test Name", last_name: "Test Last Name", email: "test@email.com", password: "TestPassword", password_confirmation: "TestPassword")
+      @user2.save
+
+      expect(@user2.errors.messages[:email]).to eq ["has already been taken"]
+    end
   end
 end
