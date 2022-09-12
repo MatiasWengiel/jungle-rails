f642948cd98af40a38dfb658d65784d0d4f59295
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 11:29:24 -0500

    Add validation that quantity is greater than or equal to zero as UI allows for negative numbers. TODO: Fix UI so that negative numbers cannot be added during creation

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
   validates :category, presence: true
   validates :description, presence: true
 end
