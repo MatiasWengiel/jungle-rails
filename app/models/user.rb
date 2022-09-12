22f817675a4c57c7ea0caab64374a9b6a0f892b8
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 13:46:18 -0500

    Add custom validation to compare :password and :password_confirmation for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password, :name, :last_name, presence: true
+  validate :compare_password_and_confirmation
+
+  private
+
+  def compare_password_and_confirmation
+    if (:password != :password_confirmation)
+      errors.add("The password and confirmation must match")
+    end
+  end
   
 end
