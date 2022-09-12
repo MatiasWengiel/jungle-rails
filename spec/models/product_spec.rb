243299e7846c5d608c8d50db24f4f35488ba0927
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 10:35:52 -0500

    Add spec to validate new product description

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -11,6 +11,14 @@ RSpec.describe Product, type: :model do
       expect(@product).to have_attributes(:name => "Test Product")
     end
 
-    
+    it 'validates the presence of a description' do
+      @category = Category.new(name: "Test Category")
+      @category.save
+      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "../../db/seed_assets/apparel1.jpg", price: 2000)
+      @product.save!
+
+      expect(@product).to have_attributes(:description => "This is a product to be tested")
+    end
+
   end
 end
