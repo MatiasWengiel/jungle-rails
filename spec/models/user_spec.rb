17728f810acf34f0fb5df82c5bbf1ae4552d2bfc
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 13:46:39 -0500

    Create test to check that new users can be created

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'validates a new user can be properly created' do
+      @user = User.new(name: "Test Name", last_name: "Test Last Name", email: "test@email.com", password: "TestPassword", password_confirmation: "TestPassword")
+  
+      expect(@user).to have_attributes(name: "Test Name", last_name: "Test Last Name", email: "test@email.com", password: "TestPassword", password_confirmation: "TestPassword")
+    end
+    
+  end
 end
