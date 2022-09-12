0d9622b4eacbb0cd35e39cac9c932535076fb439
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 11:28:56 -0500

    Modify category_id and quantity validation tests to accurately check for the presence of category_id and quantity. Add check to ensure quantity is greater than one (UI allows negative numbers). TODO: Fix UI allowing negative numbers for quantity during creation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -36,29 +36,43 @@ RSpec.describe Product, type: :model do
     it 'validates the presence of a category_id' do
       @category = Category.new(name: "Test Category")
       @category.save
-      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "not tested", price: 2000)
-      @product.save!
+      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: "", quantity: 1, image: "not tested", price: 2000)
+      @product.save
 
-      expect(@product).to have_attributes(:category_id => @category.id)
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:category]). to eq ["must exist", "can't be blank"]
     end
 
     it 'validates the presence of a quantity' do
       @category = Category.new(name: "Test Category")
       @category.save
-      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "not tested", price: 2000)
-      @product.save!
+      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: nil, image: "not tested", price: 2000)
+      @product.save
 
-      expect(@product).to have_attributes(:quantity => 1)
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:quantity]). to eq ["can't be blank", "is not a number"]
+    end
+
+    #Not on requirements from Compass, but makes sense as the current UI allows for negative numbers to be entered, which is nonsensical. TODO: Fix UI
+    it 'validates that the quantity is greater than or equal to zero' do
+      @category = Category.new(name: "Test Category")
+      @category.save
+      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: -1, image: "not tested", price: 2000)
+      @product.save
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:quantity]).to eq ["must be greater than or equal to 0"]
     end
 
     it 'validates the presence of a price' do
       @category = Category.new(name: "Test Category")
       @category.save
-      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "not tested", price: 2000)
-      @product.save!
+      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "not tested", price: nil)
+      @product.save
 
-      #Note: Price in Product creation is entered as dollars and converted to cents. Amount in Money.new is passed as cents directly (hence the extra two zeros)
-      expect(@product).to have_attributes(:price => Money.new(200000))
+
+      expect(@product).to_not be_valid
+      expect(@product.errors.message[:price]).to eq[]
     end
   end
 end
