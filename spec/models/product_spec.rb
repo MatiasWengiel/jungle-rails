7bc3bdd9a43ef34507357698c18df69e841faccd
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-12 11:16:04 -0500

    Modify description validation to accurately check for a missing validation and corresponding error

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -26,10 +26,11 @@ RSpec.describe Product, type: :model do
     it 'validates the presence of a description' do
       @category = Category.new(name: "Test Category")
       @category.save
-      @product = Product.new(name: "Test Product", description: "This is a product to be tested", category_id: @category.id, quantity: 1, image: "not tested", price: 2000)
-      @product.save!
+      @product = Product.new(name: "Test Product",  description: "", category_id: @category.id, quantity: 1, image: "not tested", price: 2000)
+      @product.save
 
-      expect(@product).to have_attributes(:description => "This is a product to be tested")
+      expect(@product).to_not be_valid
+      expect(@product.errors.messages[:description]). to eq ["can't be blank"]
     end
 
     it 'validates the presence of a category_id' do
