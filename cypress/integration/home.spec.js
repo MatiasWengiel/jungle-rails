3aab50e9fb01da8d741278865d64179a8acc4abe
Matias Wengiel <matiaswengiel@gmail.com>
2022-09-13 12:28:56 -0500

    Add test that checks for the right number of products displayed on homepage

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,11 @@
 describe('Home Page', () => {
 
-  it('visits the home page', () => {
+  it('visits the home page and finds a product', () => {
     cy.visit('/')
     cy.get(".products article").should("be.visible");
   })
 
+  it('There is 2 products on the home page', () => {
+    cy.get(".products article").should("have.length", 2)
+  })
 })
